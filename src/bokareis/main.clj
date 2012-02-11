69eb38166b475033efddf211cfdb6474334c283b
Rasmus Svensson <raek@raek.se>
2012-02-11 18:21:25 +0100

    The main program can now understand post file trees and meta files. Empty files are created at the correct locations.

--- src/bokareis/main.clj
+++ src/bokareis/main.clj
@@ -1,5 +1,47 @@
-(ns bokareis.main)
+(ns bokareis.main
+  (:require [clojure.java.io :as io]
+            [clojure.data.json :as json])
+  (:use [clojure.java.io :only [file] :rename {file f}])
+  (:import (org.joda.time DateTime)))
 
-(defn -main [blog-root]
-  nil)
+(def post-meta-file-name "post.meta")
+
+(def post-text-file-name "text.markdown")
+
+(def post-text-key "text")
+
+(def markdown-file-encoding "UTF-8")
+
+(def json-file-encoding "UTF-8")
+
+(declare list-posts read-post read-json-file relative-post-output-dir)
+
+(defn -main [root]
+  (.mkdir (f root "out"))
+  (spit (f root "out" "index.html") "")
+  (doseq [post (map read-post (list-posts (f root "posts")))
+          :let [post-out-dir (f root "out" (relative-post-output-dir post))]]
+    (.mkdirs post-out-dir)
+    (spit (f post-out-dir "index.html") "")))
+
+(defn list-posts [node]
+  (if (.exists (f node post-meta-file-name))
+    (list node)
+    (mapcat list-posts (.listFiles node))))
+
+(defn read-post [post-dir]
+  (let [meta (read-json-file (f post-dir post-meta-file-name))
+        text (slurp (f post-dir post-text-file-name) :encoding markdown-file-encoding)]
+    (assoc meta post-text-key text, "published" (DateTime/parse (get meta "published")))))
+
+(defn- read-json-file [file]
+  (with-open [in (io/reader file :encoding json-file-encoding)]
+    (json/read-json in false)))
+
+(defn relative-post-output-dir [post]
+  (let [{:strs [published slug]} post
+        year  (format "%d"   (-> published .year .get))
+        month (format "%02d" (-> published .monthOfYear .get))
+        day   (format "%02d" (-> published .dayOfMonth .get))]
+    (io/file year month day slug)))
 
--- test/bokareis/main_test.clj
+++ test/bokareis/main_test.clj
@@ -2,7 +2,8 @@
   (:use midje.sweet
         bokareis.main)
   (:use [clojure.java.io :only [file] :rename {file f}])
-  (:import (java.io File)))
+  (:import (java.io File)
+           (org.joda.time DateTime)))
 
 (defn create-temp-dir []
   (let [file (File/createTempFile "bokareis-test" "")]
@@ -34,19 +35,64 @@
              t))
   => falsey)
 
-(fact
+(defn create-post [post-dir meta-content text-content]
+  (.mkdirs post-dir)
+  (spit (f post-dir "post.meta") meta-content)
+  (spit (f post-dir "text.markdown") text-content))
+
+(fact "application renders posts"
   (with-temp-dir root
-    (.mkdir (f root "posts"))
-    (.mkdir (f root "out"))
-    (spit (f root "posts" "post.meta")
-          "{\"slug\": \"hello-world\", \"published\": \"2012-02-11T14:47:00Z\"}")
-    (spit (f root "posts" "text.markdown")
-          "Hello world")
+    (create-post (f root "posts")
+                 "{\"slug\": \"hello-world\", \"published\": \"2012-02-11T14:47:00Z\"}"
+                 "Hello world")
     (spit (f root "blog.meta")
-          "{\"posts_dir\": \"posts\", \"output_dir\": \"out\"}")
+          "{}")
     (-main (str root))
     (.exists (f root "out" "index.html"))
     => truthy
     (.exists (f root "out" "2012" "02" "11" "hello-world" "index.html"))
     => truthy))
+
+(fact "list-post finds a post"
+  (with-temp-dir root
+    (create-post (f root "a") "" "")
+    (list-posts root)
+    => [(f root "a")]))
+
+(fact "list-post finds all posts in a directory"
+  (with-temp-dir root
+    (create-post (f root "a") "" "")
+    (create-post (f root "b") "" "")
+    (create-post (f root "c") "" "")
+    (list-posts root)
+    => (just [(f root "a") (f root "b") (f root "c")] :in-any-order)))
+
+(fact "list-post finds posts regardless of directory nesting"
+  (with-temp-dir root
+    (create-post (f root "a") "" "")
+    (create-post (f root "b" "b") "" "")
+    (create-post (f root "c" "c" "c") "" "")
+    (list-posts root)
+    => (just [(f root "a") (f root "b" "b") (f root "c" "c" "c")] :in-any-order)))
+
+(fact "list-post should ignore unknown files"
+  (with-temp-dir root
+    (spit (f root "a.txt") "")
+    (list-posts root)
+    => []))
+
+(fact
+  (with-temp-dir root
+    (create-post (f root "a")
+                 "{\"slug\": \"foo-bar\", \"published\": \"2012-02-11T14:47:00Z\"}"
+                 "Foo bar.")
+    (read-post (f root "a"))
+    => {"slug" "foo-bar"
+        "published" (DateTime/parse "2012-02-11T14:47:00Z")
+        "text" "Foo bar."}))
+
+(fact
+  (relative-post-output-dir {"slug" "foo-bar"
+                             "published" (DateTime/parse "2012-02-11T14:47:00Z")})
+  => (f "2012" "02" "11" "foo-bar"))
 
